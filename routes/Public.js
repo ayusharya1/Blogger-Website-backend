1fe9822d4e1cdcd2b6a3f7f7f1910cf103f97c4e
ayush arya <ayusharya967@gmail.com>
2025-06-10 02:33:02 +0530

    new change

--- routes/Public.js
+++ routes/Public.js
@@ -1,7 +1,8 @@
 const express=require("express")
 const { GetsinglePost, Updateuser } = require("../controllers/Public")
 const upload = require("../middleware/Multer")
+const { isLogin } = require("../middleware/isAdmin")
 const PublicRoutes=express.Router()
-PublicRoutes.get("/singlepost/:id",GetsinglePost)
+PublicRoutes.get("/singlepost/:id",isLogin,GetsinglePost)
 PublicRoutes.patch("/updateuser/:id",upload.single("profile"),Updateuser)
 module.exports=PublicRoutes
